71b0a747593189015c1b91aebfd879b1d3693dab
tbdrstl <tobias.doerstel@gmail.com>
2021-11-07 20:29:38 +0100

    fixing bucks

--- src/BoundaryArray.jl
+++ src/BoundaryArray.jl
@@ -8,12 +8,13 @@ Base.size(A::PeriodicArray) = size(A.x)
 
 Base.length(A::PeriodicArray)=length(A.x)
 
-function Base.getindex(A :: PeriodicArray, I :: Vararg{Int,N}) where N
+function Base.getindex(A :: PeriodicArray{T,N}, I :: Vararg{Int,N}) where {T,N}
     I2 = size(A)
     return Base.getindex(A.x,(mod.(I .- 1,I2) .+ 1)...)
 end
 
-function Base.setindex(A :: PeriodicArray, I :: Vararg{Int,N}) where N
+function Base.setindex!(A :: PeriodicArray{T,N}, value, I :: Vararg{Int,N}) where {T,N}
+    I2 = size(A)
     return Base.setindex!(A.x,value,(mod.(I .- 1,I2) .+ 1)...)
 end
 
